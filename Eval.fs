166b3f9709dae3a8a5313253991ff6d752f4377a
Tale Stormark <102603233+talestormark@users.noreply.github.com>
2024-01-28 17:02:21 +0100

    Add files via upload

---
+++ Eval.fs
@@ -0,0 +1,34 @@
+﻿(*
+* TinyML
+* Eval.fs: evaluator
+*)
+
+module TinyML.Eval
+
+open Ast
+
+// evaluator
+//
+
+let rec eval_expr (venv : value env) (e : expr) : value =
+    match e with
+    | Lit lit -> VLit lit
+
+    | Lambda (x, _, e) -> Closure (venv, x, e)
+
+    | App (e1, e2) -> 
+        let v1 = eval_expr venv e1
+        let v2 = eval_expr venv e2
+        match v1 with
+        | Closure (venv', x, e) ->
+            let venv' = (x, v2) :: venv'
+            eval_expr venv' e
+
+        | _ -> unexpected_error "non-closure on left hand of application"
+
+    | Var x -> lookup venv x
+    
+
+    // TODO complete this implementation
+
+    | _ -> unexpected_error "eval_expr: unsupported expression: %s [AST: %A]" (pretty_expr e) e
---
+++ Typing.fs
@@ -0,0 +1,157 @@
+﻿(*
+* TinyML
+* Typing.fs: typing algorithms
+*)
+
+module TinyML.Typing
+
+open Ast
+
+let type_error fmt = throw_formatted TypeError fmt
+
+type subst = (tyvar * ty) list
+
+// TODO implement this
+let unify (t1 : ty) (t2 : ty) : subst = []
+
+// TODO implement this
+let apply_subst (t : ty) (s : subst) : ty = t
+
+// TODO implement this
+let compose_subst (s1 : subst) (s2 : subst) : subst = s1 @ s2
+
+let ($) = compose_subst
+
+// TODO implement this
+let rec freevars_ty t = Set.empty
+
+// TODO implement this
+let freevars_scheme (Forall (tvs, t)) = Set.empty
+
+// TODO implement this
+let freevars_scheme_env env = Set.empty
+
+
+// basic environment: add builtin operators at will
+//
+
+let gamma0 = [
+    ("+", TyArrow (TyInt, TyArrow (TyInt, TyInt)))
+    ("-", TyArrow (TyInt, TyArrow (TyInt, TyInt)))
+    ("*", TyArrow (TyInt, TyArrow (TyInt, TyInt)))
+    ("/", TyArrow (TyInt, TyArrow (TyInt, TyInt)))
+    ("<", TyArrow (TyInt, TyArrow (TyInt, TyBool)))
+]
+
+// type inference
+//
+
+let rec typeinfer_expr (env : scheme env) (e : expr) : ty * subst =
+    match e with
+    | Lit (LInt _) -> TyInt, []
+    | Lit (LBool _) -> TyBool, []
+    | Lit (LFloat _) -> TyFloat, [] 
+    | Lit (LString _) -> TyString, []
+    | Lit (LChar _) -> TyChar, [] 
+    | Lit LUnit -> TyUnit, []
+
+    | BinOp (e1, op, e2) ->
+        typeinfer_expr env (App (App (Var op, e1), e2))
+
+    | IfThenElse (e1, e2, Some e3) ->
+        // TODO optionally you can follow the original type inference rule and apply/compose substitutions incrementally (see Advanced Notes on ML, Table 4, page 5)
+        let t1, s1 = typeinfer_expr env e1
+        let s2 = unify t1 TyBool
+        let t2, s3 = typeinfer_expr env e2
+        let t3, s4 = typeinfer_expr env e3
+        let s5 = unify t2 t3
+        let s = s5 $ s4 $ s3 $ s2 $ s1
+        apply_subst t2 s, s
+
+
+    // TODO complete this implementation
+
+    | _ -> unexpected_error "typeinfer_expr: unsupported expression: %s [AST: %A]" (pretty_expr e) e
+
+// type checker
+//
+// optionally, a simple type checker (without type inference) could be implemented
+// you might start implementing this for simplicity and eventually write the type inference once you gain familiarity with the code
+
+let rec typecheck_expr (env : ty env) (e : expr) : ty =
+    match e with
+    | Lit (LInt _) -> TyInt
+    | Lit (LFloat _) -> TyFloat
+    | Lit (LString _) -> TyString
+    | Lit (LChar _) -> TyChar
+    | Lit (LBool _) -> TyBool
+    | Lit LUnit -> TyUnit
+
+    | Var x -> lookup env x
+
+    | Let (x, None, e1, e2) ->
+        let t1 = typecheck_expr env e1
+        let env' = (x, t1) :: env
+        typecheck_expr env' e2
+
+    | Let (x, Some t, e1, e2) ->
+        let t1 = typecheck_expr env e1
+        if t <> t1 then type_error "type %O differs from type %O in let-binding" t1 t
+        let env' = (x, t1) :: env
+        typecheck_expr env' e2
+
+    | Lambda (x, Some t, e) ->
+        let env' = (x, t) :: env
+        let te = typecheck_expr env' e
+        TyArrow (t, te)
+
+    | Lambda (x, None, e) ->
+        type_error "unannotated lambdas are not supported by the type checker"
+
+    | App (e1, e2) ->
+        let t2 = typecheck_expr env e2
+        match typecheck_expr env e1 with
+        | TyArrow (ta, tb) -> 
+            if ta <> t2 then type_error "argument has type %O while function parameter has type %O in application" t2 ta
+            tb
+        | t1 -> type_error "left hand of application is not an arrow type: %O" t1
+
+    | IfThenElse (e1, e2, Some e3) ->
+        let t1 = typecheck_expr env e1
+        if t1 <> TyBool then type_error "bool expected in if guard, but got %O" t1
+        let t2 = typecheck_expr env e2
+        let t3 = typecheck_expr env e3
+        if t2 <> t3 then type_error "then and else branches have different types: %O and %O" t2 t3
+        t2
+
+    | BinOp (e1, ("+" | "-" | "*" | "/" as op), e2) ->
+        let t1 = typecheck_expr env e1
+        if t1 <> TyInt then type_error "left hand of (%s) operator is not an int: %O" op t1
+        let t2 = typecheck_expr env e2
+        if t2 <> TyInt then type_error "right hand of (%s) operator is not an int: %O" op t2
+        TyInt
+
+    | UnOp ("not", e) ->
+        let t = typecheck_expr env e
+        if t <> TyBool then type_error "operand of not-operator is not a bool: %O" t
+        TyBool
+        
+    | BinOp (e1, "=", e2) ->
+        let t1 = typecheck_expr env e1
+        let t2 = typecheck_expr env e2
+        if t1 <> t2 then type_error "left and right hands of equality operator are different: %O and %O" t1 t2
+        TyBool
+
+    | BinOp (e1, ("<" | ">" | "<=" | ">=" as op), e2) ->
+        let t1 = typecheck_expr env e1
+        if t1 <> TyInt then type_error "left hand of (%s) operator is not an int: %O" op t1
+        let t2 = typecheck_expr env e2
+        if t2 <> TyInt then type_error "right hand of (%s) operator is not an int: %O" op t2
+        TyBool
+
+    | Tuple es -> TyTuple (List.map (typecheck_expr env) es)
+
+
+    // TODO optionally complete this implementation
+
+    | _ -> unexpected_error "typecheck_expr: unsupported expression: %s [AST: %A]" (pretty_expr e) e
